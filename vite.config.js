cf8fea1754c5682bb07715767981088db46b9f53
Rizon1326 <bsse1326@iit.du.ac.bd>
2025-04-19 21:58:03 +0600

    mehedi's part

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0', // 👈 this is important for Render
+    port: 5173       // optional, can leave out if Render assigns $PORT
+  }
 })
